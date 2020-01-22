ff3dcfeced690ea81caabf728291a6a90aad29d2
Jennifer Miller <jmillerarcher@me.com>
2020-01-22 03:27:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,11 +4,11 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    new_sudent_obj = Student.new
+    new_sudent_obj.id = row[0]
+    new_sudent_obj.name = row[1]
+    new_sudent_obj.grade = row[2]
+    new_sudent_obj
   end
 
   def self.all
